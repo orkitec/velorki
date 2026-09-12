2ef7c8cb9772d725c790b0913633a5dbf11fc79e
Steffen Roemer <steffen.roemer@orkitec.com>
2026-09-12 13:34:52 -0400

    Android: compileSdk 37 for permission_handler

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.orkitec.velorki"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
